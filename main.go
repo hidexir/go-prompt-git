67a9fdddcef57b67d1b24a13a54d41f19fb98eed
hidexir <goto_hideaki@abema.tv>
2019-02-01 11:18:42 +0900

    update

--- main.go
+++ main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"os"
+	"os/exec"
+
+	prompt "github.com/c-bata/go-prompt"
+)
+
+func executor(t string) {
+	if t == "add" {
+		cmd := exec.Command("git", "add", "-A")
+		cmd.Stdin = os.Stdin
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		cmd.Run()
+	}
+	if t == "commit" {
+		cmd := exec.Command("git", "commit")
+		cmd.Stdin = os.Stdin
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		cmd.Run()
+	}
+	return
+}
+
+func completer(t prompt.Document) []prompt.Suggest {
+	return []prompt.Suggest{
+		{Text: "add"},
+		{Text: "commit"},
+	}
+}
 
 func main() {
-	fmt.Println("hello!!")
+	p := prompt.New(
+		executor,
+		completer,
+	)
+	p.Run()
 }
